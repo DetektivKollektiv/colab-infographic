9f74bbf151ad8b1b1d76f12200f8efd669424e84
PotSchmoell <patrick96mueller@gmail.com>
2021-11-24 10:00:00 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import Intro_section from './components/Intro_section.vue';
+</script>
+
+<template>
+  <Intro_section class="h-screen w-screen"></Intro_section>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* overflow: hidden; */
+}
+::-webkit-scrollbar {
+    display: none;
+}
+</style>
---
+++ src/components/Intro_section_arrows.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  emits: ["change_slide"],
+  methods: {
+    goToNextSlide() {
+      console.log('Trying next slide');
+      this.$emit("change_slide", 1)
+    },
+    goToPrevSlide() {
+      console.log('Trying prev slide');
+      this.$emit("change_slide", -1)
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="absolute top-0 right-0">
+    <button v-on:click="goToPrevSlide" class="border bg-white m-3">zurück</button>
+    <button v-on:click="goToNextSlide" class="border bg-white m-3">weiter</button>
+  </div>
+</template>
+
+<style scoped>
+</style>
