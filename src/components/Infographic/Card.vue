219aaf9994dae8b923d0db757b1fb175b46c09df
gormlabenz <gorm-labenz@hotmail.com>
2021-12-06 17:31:29 +0100

    improved car animation

--- src/components/Infographic/Card.vue
+++ src/components/Infographic/Card.vue
@@ -20,10 +20,10 @@
         :style="{ height: height + 'px' }"
     >
         <div ref="textContainer" class="group">
-            <p class="group-hover:font-bold">
+            <p ref="textElement" class="group-hover:font-bold">
                 {{ text }}
             </p>
-            <p class="text-md pt-4" v-if="hover">
+            <p class="text-md pt-4">
                 {{ description }}
             </p>
         </div>
@@ -45,9 +45,12 @@ const props = defineProps({
 const { content } = toRefs(props)
 
 const textContainer = ref(0)
+const textElement = ref(0)
 
-function getContentHeight() {
-    return textContainer.value.scrollHeight + 32
+function getHeight() {
+    return hover.value
+        ? textContainer.value.scrollHeight + 32
+        : textElement.value.scrollHeight + 32 // 16px for the padding
 }
 
 const hover = ref(false)
@@ -68,17 +71,17 @@ const active = computed(() => {
     return content.value.storys.includes(useStoryStore().story)
 })
 
+const height = ref(0)
+
 watch([example, hover], () => {
     nextTick(() => {
-        height.value = getContentHeight()
+        height.value = getHeight()
     })
 })
 
-const height = ref(0)
-
 onMounted(() => {
     nextTick(() => {
-        height.value = getContentHeight() // 16px for the padding
+        height.value = getHeight()
     })
 })
 </script>
