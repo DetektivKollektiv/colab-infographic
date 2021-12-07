5651240f608fe5695a05138ef9d763f183875253
PotSchmoell <patrick96mueller@gmail.com>
2021-12-07 19:38:18 +0100

    Added first two slides of intro section

--- src/components/Intro/Section.vue
+++ src/components/Intro/Section.vue
@@ -25,62 +25,41 @@ export default {
         rel="stylesheet"
         type="text/css"
     />
-    <!-- <transition-group name="fade"> -->
     <transition-group
         name="custom-classes-transition"
         enter-active-class="animate__animated animate__slideInLeft"
         leave-active-class="animate__animated animate__slideOutRight"
     >
-        <!-- <h1>-Section: Currently showing slide {{ this.slide }}</h1> -->
-        <SectionSlide
-            key="Slide1"
-            text="Hallo"
-            color="bg-red-200"
-            v-if="slide == 1"
-        ></SectionSlide>
-        <SectionSlide
-            key="Slide2"
-            text="Das"
-            color="bg-blue-200"
-            v-if="slide == 2"
-        ></SectionSlide>
-        <SectionSlide
-            key="Slide3"
-            text="ist"
-            color="bg-green-200"
-            v-if="slide == 3"
-        ></SectionSlide>
-        <SectionSlide
-            key="Slide4"
-            text="ein"
-            color="bg-yellow-200"
-            v-if="slide == 4"
-        ></SectionSlide>
-        <SectionSlide
-            key="Slide5"
-            text="Slider"
-            color="bg-purple-200"
-            v-if="slide == 5"
-        ></SectionSlide>
+        <div v-if="slide == 1" class="w-screen h-screen text-white">
+            <div
+                class="w-full h-full bg-center"
+                style="background-image: url('src/assets/pictures/unsplash_RmoWqDCqN2E.png')"
+            >
+                <div class="h-full w-full sm:w-2/3 flex flex-col justify-center px-8">
+                    <h2 class="text-3xl">Lorem ipsum dolor sit</h2>
+                    <h1 class="text-purple-700 text-5xl font-bold pt-4 pb-8">Über dieses Projekt</h1>
+                    <p>Wir haben lorem ipsum dorlor sit der Prozess wird als Wertschöpfungskette dargestellt lorem ipsum</p>
+                </div>
+            </div>
+        </div>
+        <div v-if="slide == 2" class="w-screen h-screen flex flex-col sm:flex-row">
+            <div class="flex flex-col sm:w-1/2 pt-10 pl-8">
+                <h3 class="text-2xl">Lorem ipsum dolor sit</h3>
+                <h2 class="text-3xl pt-4 pb-4 sm:pb-20">Über dieses Projekt</h2>
+                <p class="sm:w-2/3">Die Kette ist der Kern, darum Mesotrends und Makrotrends lorem ipsum</p>
+            </div>
+            <div class="sm:w-1/2 h-full p-10 flex items-center justify-center">
+                <img class="max-w-full max-h-full" src="src/assets/pictures/Overview.svg">
+            </div>
+            
+        </div>
+        <div v-if="slide == 3" class="slide-container">3</div>
+        <div v-if="slide == 4" class="slide-container">4</div>
+        <div v-if="slide == 5" class="slide-container">5</div>
     </transition-group>
 
     <SectionArrows @change_slide="method_slide"></SectionArrows>
 </template>
 
 <style scoped>
-.fade-enter-active {
-}
-.fade-leave-active {
-    /* transition: opacity 0.5s ease; */
-}
-
-.fade-enter-from {
-    transition: transform 1s;
-    transition-delay: 1s;
-    transform: translateX(100px);
-}
-.fade-leave-to {
-    transition: transform 1s;
-    transform: translateX(100vw);
-}
 </style>
--- src/components/Intro/SectionArrows.vue
+++ src/components/Intro/SectionArrows.vue
@@ -15,9 +15,13 @@ export default {
 </script>
 
 <template>
-  <div class="absolute top-0 right-0">
-    <button v-on:click="goToPrevSlide" class="border bg-white m-3">zurück</button>
-    <button v-on:click="goToNextSlide" class="border bg-white m-3">weiter</button>
+  <div class="absolute top-0 right-0 flex flex-row text-xs pt-8 pr-6">
+    <button v-on:click="goToPrevSlide" class="bg-purple-700 h-5 w-5 rounded-full text-black p-2 m-1 flex items-center justify-center">
+      <i class="fas fa-arrow-left"></i>
+    </button>
+    <button v-on:click="goToNextSlide" class="bg-purple-700 h-5 w-5 rounded-full text-black p-2 m-1 flex items-center justify-center">
+      <i class="fas fa-arrow-right"></i>
+    </button>
   </div>
 </template>
 
