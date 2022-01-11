971a3f61bc230f841a9f8f7b95d373bf22cf8d13
gormlabenz <gorm-labenz@hotmail.com>
2022-01-11 11:20:38 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,18 @@
 import Section from './components/Intro/Section.vue'
 import TrendsSection from './components/TrendsSection/TrendsSection.vue';
 import Infographic from './components/Infographic/Infographic.vue'
+import Story from './components/Story/Hero.vue'
+
 </script>
 
 <template>
-    <div class="relative min-h-screen">
+    <!-- <div class="relative min-h-screen">
         <Section></Section>
     </div>
           <Intro_section></Intro_section>
-          <trends-section></trends-section>
-
-    <Infographic />
+          <trends-section></trends-section> -->
+    <Story></Story>
+    <!-- <Infographic /> -->
 </template>
 
 <style></style>
---
+++ src/components/Story/Hero.vue
@@ -0,0 +1,24 @@
+<template >
+    <div class="h-2/3 relative" v-for="story in [stories[0]]" :key="story">
+        <div class="absolute bottom-4 w-full px-4">
+            <div class="container mx-auto">
+                <h1>Hello World</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Donec euismod, nisl eget consectetur consectetur,
+                    nibh nisl consectetur nisl, eget consectetur nisl nisl
+                    eget consectetur nisl.
+                </p>
+            </div>
+        </div>
+        <img class="object-cover w-screen h-[50vh]"  :src="story.image" alt="">
+    </div>
+</template>
+<script setup>
+import stories from '@/api/stories_template.json'
+
+console.log(stories)
+</script>
+<style lang="">
+    
+</style>
