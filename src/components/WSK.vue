79b85787c413fd24e5797c70b6a55def626a0357
gormlabenz <gorm-labenz@hotmail.com>
2022-01-13 21:18:12 +0100

    removed logs

--- src/components/WSK.vue
+++ src/components/WSK.vue
@@ -1,67 +1,66 @@
 <template>
     <div>
         <div class="relative">
-            <div class="h-0.5 absolute bg-red-500 z-0 w-full">
+            <div class="w-full h-0.5 absolute bg-red-500 z-0 flex items-center">
                 <div
-                    class="flex w-full z-10 relative justify-between container-box"
+                    class="grid grid-cols-5 container-box z-10 relative items-center"
                 >
                     <div
-                        v-for="phase in phasesWithZeros"
-                        :key="phase.index"
-                        @click="emit('phaseIndex', phase.index + 1)"
+                        class="flex items-center flex-col cursor-pointer relative"
+                        v-for="(phaseText, i) in phases"
+                        :key="i"
+                        @click="emit('phaseIndex', i + 1)"
                     >
                         <div
-                            v-if="typeof phase.title == 'string'"
-                            class="flex flex-col cursor-pointer relative items-center"
+                            class="rounded-full flex justify-center items-center border-2 border-red-500 transition-all"
+                            :class="
+                                i + 1 < phaseIndex
+                                    ? 'bg-red-500 w-4 h-4 md:w-8 md:h-8 text-white label-sm'
+                                    : i + 1 > phaseIndex
+                                    ? 'bg-white w-4 h-4 md:w-8 md:h-8 label-sm'
+                                    : 'w-8 h-8 md:w-12 md:h-12  label-xl bg-red-500 text-white'
+                            "
                         >
-                            <div
-                                class="rounded-full flex justify-center items-center border-2 border-red-500 transition-all"
+                            <span
                                 :class="
-                                    phase.index + 1 < phaseIndex
-                                        ? 'bg-red-500 w-4 h-4 md:w-8 md:h-8 -mt-2 md:-mt-4 text-white label-sm'
-                                        : phase.index + 1 > phaseIndex
-                                        ? 'bg-white w-4 h-4 md:w-8 md:h-8 -mt-2 md:-mt-4 label-sm'
-                                        : 'w-8 h-8 md:w-12 md:h-12 -mt-4 md:-mt-6  label-xl bg-red-500 text-white'
+                                    i + 1 !== phaseIndex
+                                        ? 'hidden md:block'
+                                        : ''
                                 "
                             >
-                                <span
-                                    :class="
-                                        phase.index + 1 !== phaseIndex
-                                            ? 'hidden md:block'
-                                            : ''
-                                    "
-                                >
-                                    {{ phase.index + 1 }}
-                                </span>
-                            </div>
-                            <div class="text-center">
-                                <p
-                                    v-if="showTitle"
-                                    class="transition-all"
-                                    :class="[
-                                        phase.index + 1 == phaseIndex
-                                            ? 'label-xl mt-6 md:mt-8'
-                                            : 'label-sm mt-6',
-                                        phase.index + 1 !== phaseIndex
-                                            ? 'hidden md:block'
-                                            : '',
-                                    ]"
-                                >
-                                    {{ phase.title }}
-                                </p>
-                                <p v-if="showDescription" class="w-3/4">
-                                    Lorem ipsum, dolor sit amet consectetur
-                                    adipisicing elit. Aliquam illo maxime fugiat
-                                    corrupti adipisci.
-                                </p>
-                            </div>
+                                {{ i + 1 }}
+                            </span>
+                            <div
+                                v-if="i + 1 !== phases.length"
+                                class="arrow-right absolute right-0 top-1/2 -translate-y-1/2 translate-x-1/2"
+                            ></div>
                         </div>
-
-                        <i
-                            v-else
-                            class="fas fa-caret-right text-4xl text-red-500 -translate-y-1/2"
-                        ></i>
                     </div>
+                </div>
+            </div>
+            <div class="grid grid-cols-5 container-box">
+                <div
+                    class="flex items-center flex-col text-center cursor-pointer"
+                    v-for="(phaseText, i) in phases"
+                    :key="i"
+                    @click="emit('phaseIndex', i + 1)"
+                >
+                    <p
+                        v-if="showTitle"
+                        class="transition-all"
+                        :class="[
+                            i + 1 == phaseIndex
+                                ? 'label-xl mt-6 md:mt-8'
+                                : 'label-sm mt-6',
+                            i + 1 !== phaseIndex ? 'hidden md:block' : '',
+                        ]"
+                    >
+                        {{ phaseText }}
+                    </p>
+                    <p v-if="showDescription" class="w-3/4">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Aliquam illo maxime fugiat corrupti adipisci.
+                    </p>
                 </div>
             </div>
         </div>
@@ -82,9 +81,7 @@ const props = defineProps({
         default: false,
     },
 })
-
 const { phaseIndex, showTitle, showDescription } = toRefs(props)
-
 const phases = [
     'Initiieren',
     'Produzieren',
@@ -92,19 +89,6 @@ const phases = [
     'Verbreiten',
     'Beeinflussung',
 ]
-
-function insertZeros(array) {
-    return array.reduce((acc, title, i) => {
-        acc.push({ title, index: i })
-        if (i + 1 !== array.length) {
-            acc.push(0)
-        }
-        return acc
-    }, [])
-}
-
-const phasesWithZeros = insertZeros(phases)
-
 const emit = defineEmits(['phaseIndex'])
 </script>
 
