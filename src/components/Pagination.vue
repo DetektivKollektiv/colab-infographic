1ee7fa200ae2ab77db29127c7125ed89e9b716f8
gormlabenz <gorm-labenz@hotmail.com>
2022-01-31 11:56:25 +0100

    changed pagination gap to space

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex gap-2 md:gap-6 text-red-500 text-2xl items-center">
+    <div class="flex space-x-2 md:space-x-6 text-red-500 text-2xl items-center">
         <button class="hidden md:block" @click="emit('index', index - 1)">
             <i v-if="showArrows" class="fas fa-caret-left"></i>
         </button>
